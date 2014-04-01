9be67b1b0876aa0b1ce02778c1fc5a12a3b2eaa2
Hervé BREDIN <bredin@limsi.fr>
2014-04-01 16:50:07 +0200

    chore: spring cleaning

--- test/postMedia.js
+++
@@ -1,6 +0,0 @@
-jQuery.post("/corpus/id", {
-  	"id": "dd",  
-	"name" : "ab"
-}, function(data, textStatus, jqXHR) { 
-    console.log("Post resposne:"); console.dir(data); console.log(textStatus); console.dir(jqXHR); 
-});
