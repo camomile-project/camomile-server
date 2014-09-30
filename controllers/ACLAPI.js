1d0ea867bbda2fa89d7bbb1845e764780c3fc295
juandelamontana <poignant@Anh-Phuongs-MacBook-Pro.local>
2014-09-30 15:53:59 +0200

    correction retour json

--- controllers/ACLAPI.js
+++ controllers/ACLAPI.js
@@ -27,59 +27,42 @@ SOFTWARE.
 */
 
 var Corpus = require('../models/Corpus').Corpus;
-
 var Media = require('../models/Media').Media; //get the media model
-
 var Layer = require('../models/Layer').Layer; //get the layer model
-
 var Annotation = require('../models/Annotation').Annotation; //get the annotation model
-
 var ACL = require('../models/ACL').ACL;
-
 var User = require('../models/user').User;
-
 var Group = require('../models/Group').Group;
 
 // retrieve all acl
 exports.listAll = function(req, res){
 	ACL.find({}, function(error, data){
 		if(error) throw error;
-		else
-			res.json(data);
+		else res.status(200).json(data);
 	});
 }
 
 // retrieve an acl id
 exports.listWithId = function(req, res){
 	ACL.findById(req.params.id, function(error, data){
-		if(error){
-			res.json(error);
-		}
-		else if(data == null){
-			res.json('no such id!')
-		}
-		else
-			res.json(data);
+		if(error) res.status(400).json(error);
+		else if(data == null) res.status(400).json({error:'no such id!'})
+		else res.status(200).json(data);
 	});
 };
+
 //retrieve an ACL id
 exports.listWithIdOfResource = function(req, res){
 	var idSought = req.params.id_anno;///corpus/:id_corpus/media/:id_media/layer/:id_layer/annotation/:id_anno/acl
 	if(idSought == undefined) idSought = req.params.id_layer;
 	if(idSought == undefined) idSought = req.params.id_media;
 	if(idSought == undefined) idSought = req.params.id;
-	
-	if(idSought == undefined) return res.send(404, "The id has not been filled up");
+	if(idSought == undefined) return res.status(400).json({error: "The id has not been filled up"});
 	
 	ACL.findOne({id:idSought}, function(error, data){
-		if(error){
-			res.json(error);
-		}
-		else if(data == null){
-			res.json('no such id!')
-		}
-		else
-			res.json(data);
+		if(error) res.status(400).json(error);
+		else if(data == null) res.status(400).json('no such id!')
+		else res.status(200).json(data);
 	});
 };
 
@@ -89,45 +72,31 @@ exports.updateWithIdOfResource = function(req, res){
 	if(idSought == undefined) idSought = req.params.id_layer;
 	if(idSought == undefined) idSought = req.params.id_media;
 	if(idSought == undefined) idSought = req.params.id;
-	if(idSought == undefined) return res.send(404, "The id has not been filled up");
+	if(idSought == undefined) return res.status(400).json({error: "The id has not been filled up"});
 	
 	ACL.findOne({id:idSought}, function(error, data){
-		if(error){
-			res.json(error);
-		}
-		else if(data == null){
-			res.json('no such id!');
-		}
-		else {
-			
-			var username = req.body.username,
-				userright = req.body.userright,
-				groupname = req.body.groupname,
-				groupright = req.body.groupright;
+		if(error) res.status(400).json(error);
+		else if(data == null) res.status(400).json({error:'no such id!'});
+		else {			
+			var username = req.body.username;
+			var userright = req.body.userright;
+			var groupname = req.body.groupname;
+			var groupright = req.body.groupright;
 				
 			if(username != undefined && userright != undefined) {
 				User.findOne({username : {$regex : new RegExp('^'+ username + '$', "i")}}, function(error, datU){
 					if(error) send(error);
-					else 
-					{
-						if(datU == null){
-							console.log("This group does not exist");
-							res.send("This group does not exist");
-						}
+					else {
+						if(datU == null) res.status(400).json({error:"This group does not exist"});
 						else {
 							var i = findUserInUsersRightArr(username, data.users);
-							if(i == -1)
-								data.users.push({login : username, right : userright});
+							if(i == -1) data.users.push({login : username, right : userright});
 							else data.users[i].right = userright;
 							data.save(function(error, dat){
-								if(error){
-									res.send(404, error);
-								}
-								else {
-									res.json(dat);
-								}
+								if(error) res.status(400).json(error);
+								else res.status(200).json(dat);
 							});
-						} //else
+						} 
 					}
 				});	
 			}			
@@ -135,32 +104,23 @@ exports.updateWithIdOfResource = function(req, res){
 			else if(groupname != undefined && groupright != undefined) {
 				Group.findOne({groupname : {$regex : new RegExp('^'+ groupname + '$', "i")}}, function(error, datG){
 					if(error) send(error);
-					else 
-					{
-						if(datG == null){
-							console.log("This group does not exist");
-							res.send("This group does not exist");
-						}
+					else {
+						if(datG == null) res.status(400).json({error:"This group does not exist"});
 						else {
 							var i = findUserInUsersRightArr(groupname, data.groups);
-							if(i == -1)
-								data.groups.push({login : groupname, right : groupright});
+							if(i == -1)	data.groups.push({login : groupname, right : groupright});
 							else data.groups[i].right = groupright;
 							
 							data.save(function(error, dat){
-								if(error){
-									res.send(404, error);
-								}
-								else {
-									res.json(dat);
-								}
+								if(error)res.status(400).json(error);
+								else res.status(200).json(dat);
 							});
-						} //else
+						} 
 					}
 				});	
 			}
-			else return res.send(404, "not found groupname or username or userright");
-		} //else
+			else return res.status(400).json({error: "not found groupname or username or userright"});
+		} 
 	});
 };
 
@@ -177,46 +137,27 @@ exports.getRightAllId = function(callback){
 exports.addUserRightGeneric = function addUserRightGeneric(id1, userLogin, userRight){
 	User.findOne({username : {$regex : new RegExp('^'+ userLogin + '$', "i")}}, function(error, data){
 		if(error) throw error;
-		else 
-		{
-			if(data == null){
-				console.log("This user does not exist");
-			}
-			else {
-		
+		else {
+			if(data == null) res.status(400).json({error:"This user does not exist"});
+			else {		
 				ACL.findOne({id:id1}, function(error, dataACL){
-					if(error){
-						throw error;
-					}
+					if(error) throw error;
 					else if(dataACL == null || dataACL.length == 0){
 						//not exist, create a new entry
-						var item = {
-							"id" : id1,
-							"users" : [],
-							"groups" : []
-						};
+						var item = {"id" : id1, "users" : [], "groups" : []};
 						item.users.push({login : userLogin, right : userRight});
-		
 						var aclItem = new ACL(item);
-
-						aclItem.save(function(err){
-							if(err) { throw err; }
+						aclItem.save(function(err){	
+							if(err) throw err; 
 						});
 					}
-					else {
-						// just update the user account
+					else {																							// just update the user account						
 						var i = findUserInUsersRightArr(userLogin, dataACL.users);
-						if(i == -1)
-							dataACL.users.push({login : userLogin, right : userRight});
+						if(i == -1)	dataACL.users.push({login : userLogin, right : userRight});
 						else dataACL.users[i].right = userRight;
 						
-						//data.users.push({login : userLogin, right : userRight});
 						dataACL.save(function(error, dat){
-							if(error){
-								throw error;
-							}
-							else {
-							}
+							if(error) throw error;
 						});
 					}
 				});
@@ -234,54 +175,33 @@ function findUserInUsersRightArr(login, users){
 
 // this function is to add a user right
 exports.addUserRight = function(req, res){
-	if(req.body.userLogin == undefined || req.body.userRight == undefined)
-		return res.send(404, "one or more data fields are not filled out properly");
+	if(req.body.userLogin == undefined || req.body.userRight == undefined) return res.status(400).json({error: "one or more data fields are not filled out properly"});
 		
 	User.findOne({username : {$regex : new RegExp('^'+ req.body.userLogin + '$', "i")}}, function(error, data){
 		if(error) throw error;
-		else 
-		{
-			if(data == null){
-				console.log("This user does not exist");
-				res.send(404, "This user does not exist");
-			}
-			else {
-		
+		else {
+			if(data == null) res.status(400).json({error: "This user does not exist"});
+			else {		
 				ACL.findOne({id:req.body.id}, function(error, data){
-					if(error){
-						throw error;
-					}
+					if(error) throw error;
 					else if(data == null){
 						//not exist, create a new entry
-						var item = {
-							"id" : req.body.id,
-							"users" : [],
-							"groups" : []
-						};
+						var item = { "id" : req.body.id, "users" : [], "groups" : []};
 						item.users.push({login : req.body.userLogin, right : req.body.userRight});
-		
 						var aclItem = new ACL(item);
-
 						aclItem.save(function(err, acl){
-							if(err) { throw err; }
-							
-							res.send(acl);
+							if(err) throw err;
+							res.status(200).json(acl);
 						});
 					}
-					else {
-						// just update the user account
+					else {																				// just update the user account
 						var i = findUserInUsersRightArr(req.body.userLogin, data.users);
-						if(i == -1)
-							data.users.push({login : req.body.userLogin, right : req.body.userRight});
+						if(i == -1) data.users.push({login : req.body.userLogin, right : req.body.userRight});
 						else data.users[i].right = req.body.userRight;
 						
 						data.save(function(error, dat){
-							if(error){
-								throw error;
-							}
-							else {
-								res.send(dat);
-							}
+							if(error) throw error;
+							else res.status(200).json(dat);
 						});
 					}
 				});
@@ -292,157 +212,107 @@ exports.addUserRight = function(req, res){
 
 // grant group right to the resource
 exports.addGroupRight = function(req, res){
-	if(req.body.groupLogin == undefined || req.body.groupRight == undefined)
-		return res.send(404, "one or more data fields are not filled out properly");
+	if(req.body.groupLogin == undefined || req.body.groupRight == undefined) return res.status(400).json({error: "one or more data fields are not filled out properly"});
 		
 	Group.findOne({groupname : {$regex : new RegExp('^'+ req.body.groupLogin + '$', "i")}}, function(error, data){
 		if(error) throw error;
-		else 
-		{
-			if(data == null){
-				console.log("This group does not exist");
-				res.send("This group does not exist");
-			}
+		else {
+			if(data == null) res.status(400).json({error:"This group does not exist"});
 			else {
-				
 				ACL.findOne({id:req.body.id}, function(error, data){
-					if(error){
-						throw error;
-					}
-					else if(data == null){
-						//not exist, create a new entry
-						var item = {
-							"id" : req.body.id,
-							"users" : [],
-							"groups" : []
-						};
+					if(error) throw error;
+					else if(data == null){						//not exist, create a new entry
+						var item = {"id" : req.body.id,	"users" : [], "groups" : []};
 						item.groups.push({login : req.body.groupLogin, right : req.body.groupRight});
-			
 						var aclItem = new ACL(item);
-
 						aclItem.save(function(err, dat){
-							if(err) { throw err; }
-							res.send(dat);
+							if(err) throw err;
+							res.status(200).json(dat);
 						});
 					}
-					else {
-						// just update the group account
+					else {																							// just update the group account
 						var i = findUserInUsersRightArr(req.body.groupLogin, data.groups);
-
-						if(i == -1)
-							data.groups.push({login : req.body.groupLogin, right : req.body.groupRight});
+						if(i == -1) data.groups.push({login : req.body.groupLogin, right : req.body.groupRight});
 						else data.groups[i].right = req.body.groupRight;
-						
-						
 						data.save(function(error, dat){
-							if(error){
-								throw error;
-							}
-							else{
-								res.send(dat);
-							}
+							if(error) throw error;
+							else res.status(200).json(dat);
 						});
-					} //else
+					} 
 				});			
 				
-			} //else
-		} //first else
-	}); // group.find
+			} 
+		} 
+	}); 
 }
 
 
 exports.addGroupRightGeneric = function addGroupRightGeneric(id1, groupLogin, groupRight){
 	Group.findOne({groupname : {$regex : new RegExp('^'+ groupLogin + '$', "i")}}, function(error, data){
 		if(error) throw error;
-		else 
-		{
-			if(data == null){
-				console.log("This group does not exist");
-			}
-			else {
-				
+		else {
+			if(data == null) res.status(400).json({error:"This group does not exist"});
+			else {				
 				ACL.findOne({id:id1}, function(error, data){
-					if(error){
-						throw error;
-					}
-					else if(data == null || data.length == 0){
-						//not exist, create a new entry
-						var item = {
-							"id" : id1,
-							"users" : [],
-							"groups" : []
-						};
+					if(error) throw error;
+					else if(data == null || data.length == 0){								//not exist, create a new entry
+						var item = {"id" : id1, "users" : [], "groups" : []};
 						item.groups.push({login : groupLogin, right : groupRight});
-			
 						var aclItem = new ACL(item);
-
 						aclItem.save(function(err){
-							if(err) { throw err; }
+							if(err) throw err;
 						});
 					}
-					else {
-						// just update the group account
+					else {																	// just update the group account
 						var i = findUserInUsersRightArr(groupLogin, data.groups);
-
-						if(i == -1)
-							data.groups.push({login : groupLogin, right : groupRight});
+						if(i == -1)	data.groups.push({login : groupLogin, right : groupRight});
 						else data.groups[i].right = groupRight;
-						
-						//data.groups.push({login : groupLogin, right : groupRight});
 						data.save(function(error, data){
-							if(error){
-								throw error;
-							}
-							else{
-
-							}
+							if(error) throw error;
 						});
 					}
 				});			
 				
-			} //else
-		} //first else
-	}); // group.find
+			} 
+		} 
+	}); 
 }
+
 // remove an ACL entry
 exports.removeAnACLEntry = function removeAnACLEntry(id2remove){
 	ACL.remove({id : id2remove}, function(error, data){
 		if(error) {
-			console.log(error); return;
+			res.status(400).json(error); 
+			return;
 		}
-		else {
-			console.log("just removed the " + id2remove + " from the ACL");
-		}
+		else res.status(200).json({message :"removed the " + group2remove + " from the ACL"});
 	});
 }
 
 function removeItemFromArray(a, item){
 	var index = -1;
-
 	for(var j = 0; j < a.length; j++) {
 		if(a[j].login == item.toLowerCase()) {
-			index = j; break;
+			index = j; 
+			break;
 		}
 	}	
-	if(index != -1)
-		a.splice(index, 1);
+	if(index != -1)	a.splice(index, 1);
 }
 
 // remove a user from acl
 exports.removeAUserFromALC = function removeAnUserFromALC(user2remove){
 	ACL.find({'users.login' : {$regex : new RegExp('^'+ user2remove + '$', "i")}}, function(error, data){
 		if(error){
-			console.log(error); return;
+			res.status(400).json(error); 
+			return;
 		}
 		else if(data != null) {
 			for(var i = 0; i < data.length; i++) {
 				removeItemFromArray(data[i].users, user2remove);
-				
 				data[i].save(function(error, dat) {
 					if(error) throw error;
-					else {
-						console.log("just removed the " + user2remove + " from the ACL");
-					}
+					else res.status(200).json({message :"removed the " + group2remove + " from the ACL"});
 				});
 			}
 		}
@@ -452,17 +322,15 @@ exports.removeAUserFromALC = function removeAnUserFromALC(user2remove){
 exports.removeAGroupFromALC = function removeAnGroupFromALC(group2remove){
 	ACL.find({'groups.login' : {$regex : new RegExp('^'+ group2remove + '$', "i")}}, function(error, data){
 		if(error){
-			console.log(error); return;
+			res.status(400).json(error); 
+			return;
 		}
 		else if(data != null) {
 			for(var i = 0; i < data.length; i++) {
 				removeItemFromArray(data[i].groups, group2remove);
-				
 				data[i].save(function(error, dat) {
 					if(error) throw error;
-					else {
-						console.log("just removed the " + group2remove + " from the ACL");
-					}
+					else res.status(200).json({message :"removed the " + group2remove + " from the ACL"});
 				});
 			}
 		}
--- controllers/LayerAPI.js
+++ controllers/LayerAPI.js
@@ -37,7 +37,6 @@ var ACL = require('../models/ACL').ACL,
 	compound = require('../controllers/CompoundAPI'),
 	commonFuncs = require('../lib/commonFuncs');
 
-// for the uri : app.get('/corpus/:id_corpus/media/:id_media/layer', 
 /*
 	- First: retrieves all layers regardless of user/group's rights
 	- Second: finds all groups belonging to the connected user
@@ -46,20 +45,14 @@ var ACL = require('../models/ACL').ACL,
 */
 exports.listAll = function(req, res){
 	function final(resultReturn, n) { 
-		if(resultReturn.length == 0 && n > 0)										
-			res.json(403, "You dont have enough permission to get this resource");
-		else res.json(resultReturn);
+		if(resultReturn.length == 0 && n > 0) res.status(403).json({error:"You dont have enough permission to get this resource"});
+		else res.status(200).json(resultReturn);
 	}
-	//find all layers under this media
-	Layer.find({id_media : req.params.id_media}, function(error, data){
-		if(error){
-			res.json(error);
-		}
+	Layer.find({id_media : req.params.id_media}, function(error, data){																		//find all layers under this media
+		if(error) res.status(400).json(error);
 		else {
 			var connectedUser = req.session.user;
-			if(GLOBAL.no_auth == true || (connectedUser != undefined && connectedUser.role == "admin")){
-				res.json(data);
-			}
+			if(GLOBAL.no_auth == true || (connectedUser != undefined && connectedUser.role == "admin")) res.status(400).json(data);
 			else if(connectedUser != undefined && data != null){
 				//first find all groups to which the connecteduser belongs
 				Group.find({'usersList' : {$regex : new RegExp('^'+ connectedUser.username + '$', "i")}}, function(error, dataGroup) {
@@ -67,13 +60,9 @@ exports.listAll = function(req, res){
 					else {
 						result = [];
 						resultReturn = [];
-
-						for(var i = 0; i < data.length; i++){
-							result.push(data[i]._id);
-						}
-						// find all acls of these id
-						ACL.find({id:{$in:result}}, function(error, dataACL){
-							if(error) console.log("error in ACL-corpusListall:");
+						for(var i = 0; i < data.length; i++) result.push(data[i]._id);
+						ACL.find({id:{$in:result}}, function(error, dataACL){																// find all acls of these id
+							if(error) res.status(500).json({error:"error in ACL-corpusListall:"});
 							else if(dataACL != null) {
 								var dataACLLen = dataACL.length;
 								var countTreatedACL = 0;
@@ -84,8 +73,8 @@ exports.listAll = function(req, res){
 											resultReturn.push(data[i]);
 											countTreatedACL += 1; 
 										}
-									} //not found this user's right on the current resource, look for its group's one
-									else {
+									} 
+									else {																									//not found this user's right on the current resource, look for its group's one
 										foundPos = commonFuncs.findUsernameInGroupACL(dataGroup, dataACL[i].groups);
 										if(foundPos != -1) {
 											if(dataACL[i].groups[foundPos].right != 'N') {
@@ -93,97 +82,77 @@ exports.listAll = function(req, res){
 												countTreatedACL += 1; 
 											}
 										}
-										else { //not found user right, nor group one, do a back propagation
-
+										else { 														//not found user right, nor group one, do a back propagation
 											(function(d){
 												parentID = [];
-												parentID.push(req.params.id_media); parentID.push(req.params.id_corpus);
+												parentID.push(req.params.id_media); 
+												parentID.push(req.params.id_corpus);
 												ACL.find({id:{$in:parentID}}, function(error, dataACL1){
-													if(error) res.send(error);
+													if(error) res.status(400).json(error);
 													else if(dataACL1 != null) {
 														countTreatedACL += 1;
 														var contd = true;
 														for(var j = 0; j < dataACL1.length && contd; j++) {
-															var foundPos = commonFuncs.findUsernameInACL(connectedUser.username, dataACL1[j].users);
-														
+															var foundPos = commonFuncs.findUsernameInACL(connectedUser.username, dataACL1[j].users);														
 															if(foundPos != -1) {
 																if(dataACL1[j].users[foundPos].right != 'N') {
-																	resultReturn.push(d); contd = false;
+																	resultReturn.push(d); 
+																	contd = false;
 																}
 															}
 															else {
-																foundPos = commonFuncs.findUsernameInGroupACL(dataGroup, dataACL1[j].groups);
-							
+																foundPos = commonFuncs.findUsernameInGroupACL(dataGroup, dataACL1[j].groups);							
 																if(foundPos != -1) {
 																	if(dataACL1[j].groups[foundPos].right != 'N') {
-																		resultReturn.push(d); contd = false;
+																		resultReturn.push(d); 
+																		contd = false;
 																	}
-																	else contd = false; // stop because we already found the right N
+																	else contd = false; 							// stop because we already found the right N
 																}
 															}
-														} //for
+														} 
 														if(countTreatedACL == dataACLLen) {
 															countTreatedACL = -1
 															final(resultReturn, data.length);
 														}
-													} //else if(dataACL1 != null)
-												}); //acl 2
-											})(data[i]); // treat the callback function
-										} // else { //not found user right	
-									} //else
-								} //for
-								if(countTreatedACL == dataACLLen) 
-									final(resultReturn, data.length);
-							} //else if(dataACL != null)
-							else {
-								res.json(404, "error in finding acl");
-							}
-						}); //ACL.find
-					} //else
-				}); // group
-				//res.json(resultReturn);
-			} // else if (connectedUser)
+													} 
+												}); 
+											})(data[i]); 
+										}
+									} 
+								} 
+								if(countTreatedACL == dataACLLen) final(resultReturn, data.length);
+							} 
+							else res.status(400).json({error: "error in finding acl"});
+						}); 
+					} 
+				});
+				
+			} 
 			else {
-				if(data != null)
-					res.json(403, "You dont have permission to access this resource"); 
+				if(data != null) res.status(400).json({error: "You dont have permission to access this resource"}); 
 				else return([]);
 			}
 		}
 	});
 }
 
-//for the uri: app.get('/corpus/:id_corpus/media/:id_media/layer/:id_layer 
 exports.listWithId = function(req, res){
 	Layer.findById(req.params.id_layer, function(error, data){
-		if(error){
-			res.json(error);
-		}
-		else if(data == null){
-			res.json('no such id_layer!')
-		}
-		else
-			res.json(data);
+		if(error) res.status(400).json(error);
+		else if(data == null) res.status(400).json({error:'no such id_layer!'})
+		else res.status(200).json(data);
 	});
 }
 
-//test for Posting corpus
-//app.post('/corpus/:id_corpus/media/:id_media/layer'
-/*
-	if body.annotation is given, a list of annotations will be created
-*/
 exports.post = function(req, res){
-	if(req.body.layer_type == undefined || req.body.fragment_type == undefined 
-		|| req.body.data_type == undefined || req.body.source == undefined)
-		return res.send(404, "one or more data fields are not filled out properly");
-	if(req.body.annotation != undefined)
-		return compound.postAll(req, res);
+	if(req.body.layer_type == undefined || req.body.fragment_type == undefined || req.body.data_type == undefined || req.body.source == undefined) return res.status(400).json({error: "one or more data fields are not filled out properly"});
+	if(req.body.annotation != undefined) return compound.postAll(req, res);
 	else {
 		Media.findById(req.params.id_media, function(error, data){
-			if(error){
-				res.json(error);
-			}
+			if(error) res.status(400).json(error);
 			else if(data == null){
-				res.json('Could not post this layer because the given id_media is incorrect'); 
+				res.status(400).json({error:'Could not post this layer because the given id_media is incorrect'}); 
 				return;
 			}
 			else {
@@ -197,11 +166,9 @@ exports.post = function(req, res){
 				};
 			
 				var connectedUser = "root";
-				if(req.session.user)
-					connectedUser = req.session.user.username;
+				if(req.session.user) connectedUser = req.session.user.username;
 
-				// add new layer
-				var layer = new Layer(layer_data);
+				var layer = new Layer(layer_data);																	// add new layer
 
 				var modified = {
 					"id_media" : req.params.id_media,
@@ -210,20 +177,16 @@ exports.post = function(req, res){
 					"data_type" : req.body.data_type,
 					"source" : req.body.source
 				};
-				// update history
-				layer.history.push({name : connectedUser, date : new Date(), modification: modified});
-				// save new layer	
-				layer.save( function(errorLayer, dataLayer){
+
+				layer.history.push({name : connectedUser, date : new Date(), modification: modified});				// update history
+				layer.save( function(errorLayer, dataLayer){														// save new layer	
 					if(errorLayer){
-						console.log('error in posting layer data, the id_media does not exist');
-						res.json(errorLayer);
+						res.status(400).json(errorLayer);
 						return;
 					}
 					else{
-						console.log('Success on saving layer data');
-						// add the current id to the ACL list
-						ACLAPI.addUserRightGeneric(dataLayer._id, connectedUser, 'A');
-						res.json(dataLayer);
+						ACLAPI.addUserRightGeneric(dataLayer._id, connectedUser, 'A');								// add the current id to the ACL list
+						res.status(200).json(dataLayer);
 					}
 				});
 			}
@@ -231,47 +194,27 @@ exports.post = function(req, res){
 	}
 }
 
-//test for updating layers
-//app.put('/corpus/:id_corpus/media/:id_media/layer/:id_layer', 
 exports.updateAll = function(req, res){
-	if(req.body.layer_type == undefined && req.body.fragment_type == undefined 
-		&& req.body.data_type == undefined && req.body.source == undefined)
-		return res.send(404, "one or more data fields are not filled out properly");
+	if(req.body.layer_type == undefined && req.body.fragment_type == undefined && req.body.data_type == undefined && req.body.source == undefined) return res.status(400).json({error:"one or more data fields are not filled out properly"});
 
 	var update = {};
-	if(req.body.id_media)
-		update.id_media = req.params.id_media;
-	if(req.body.layer_type)
-		update.layer_type = req.body.layer_type;
-	if(req.body.fragment_type)
-		update.fragment_type = req.body.fragment_type;
-	if(req.body.data_type)
-		update.data_type = req.body.data_type;
-	if(req.body.source)
-		update.source = req.body.source;
+	if(req.body.id_media) update.id_media = req.params.id_media;
+	if(req.body.layer_type) update.layer_type = req.body.layer_type;
+	if(req.body.fragment_type) update.fragment_type = req.body.fragment_type;
+	if(req.body.data_type) update.data_type = req.body.data_type;
+	if(req.body.source) update.source = req.body.source;
 			
 	Layer.findByIdAndUpdate(req.params.id_layer, update, function (error, oneLayer) {
-		if(error){
-			res.json(error);
-		}
-		else
-		{
+		if(error) res.status(400).json(error);
+		else {
 			var dateNow = new Date();
 			var uname = "root";
 			if(req.session.user) uname = req.session.user.username;
-			
 			oneLayer.history.push({name:uname, date: dateNow, modification: update});
-			
 			oneLayer.save( function(error, data){
-				if(error){
-					res.json(error);
-				}
-				else{
-					res.json(data);
-				}
+				if(error) res.status(400).json(error);
+				else res.status(200).json(data);
 			});
-			//res.json(data);
 		}
 	});
 }
-
