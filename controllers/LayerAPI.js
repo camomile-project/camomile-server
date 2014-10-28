14120af44c21e5fd6057b5c59fdfa3f62e846297
Johann Poignant <poignant@Anh-Phuongs-MacBook-Pro.local>
2014-10-28 16:46:27 +0100

    add update

--- controllers/LayerAPI.js
+++ controllers/LayerAPI.js
@@ -115,3 +115,41 @@ exports.remove = function (req, res) {
 		else res.status(400).json({message:error});
 	});
 }
+
+//update information of a layer
+exports.update = function(req, res){
+	var update = {};
+	var history = {};
+	var error=null;
+	async.waterfall([
+		function(callback) {
+			if (req.body.name) {
+				if (req.body.name == "") error = "name can't be empty";
+				else {
+					update.name = req.body.name;
+					history.name = req.body.name;
+				}
+			}				
+			if (req.body.description) {
+				update.description = req.body.description;
+				history.description = req.body.description;
+			}
+			callback(error, update);
+		},
+		function(update, callback) {
+			Layer.findById(req.params.id_layer, function(error, layer){
+				update.history = layer.history;
+				update.history.push({date:new Date(), id_user:req.session.user._id, modification:history})
+				callback(error, update);
+			});
+		},
+		function(update, callback) {
+			Layer.findByIdAndUpdate(req.params.id_layer, update, function (error, layer) {
+				if (error) res.status(400).json(error);
+				else res.status(200).json(layer);
+			});
+		},
+	], function (error, trueOrFalse) {
+		if (error) res.status(400).json({message:error});
+	});
+}
