734dfabd0c452e816d85cc1830127c4930e9c765
juandelamontana <poignant@Anh-Phuongs-MacBook-Pro.local>
2014-10-28 12:00:08 +0100

    suppression vielle fct

--- controllers/MediaAPI.js
+++ controllers/MediaAPI.js
@@ -22,104 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-/* The API controller
-   Exports 3 methods:
-   * post - Creates a new media
-   * listAll - Returns a list of media
-   * listWithId - Returns a specific media of a given id
-*/
-
-
 var fileSystem = require('fs'); //working with video streaming
-
-// for the uri : app.get('/corpus/:id/media'
-/*
-	- First: retrieves all media regardless of user/group's rights
-	- Second: finds all groups belonging to the connected user
-	- For each found media, check ACLs (the permission of the connected user and its groups)
-	- If not found, check ACLs of the related corpus 
-*/
-exports.listAll = function(req, res){	
-	function final(resultReturn, n) { 
-		if (resultReturn.length == 0 && n > 0) res.status(403).json({error: "You dont have enough permission to get this resource"});
-		else res.status(200).json(resultReturn);
-	}
-	//find all media under this corpus
-	Media.find({id_corpus : req.params.id}, function(error, data){
-		if (error) res.status(400).json({error:"error", message:error});
-		else {
-			var connectedUser = req.session.user;
-			if (GLOBAL.no_auth == true || (connectedUser != undefined && connectedUser.role == "admin")) res.status(200).json(data);
-			else if (connectedUser != undefined && data != null){
-				//first find groups to which the connected user belongs
-				Group.find({'usersList' : {$regex : new RegExp('^'+ connectedUser.username + '$', "i")}}, function(error2, dataGroup) {
-					if (error2) res.status(400).json({error:"error", message:error2});
-					else {
-						result = [];
-						resultReturn = [];
-						for(var i = 0; i < data.length; i++) result.push(data[i]._id);
-						ACL.find({id:{$in:result}}, function(error3, dataACL){										// find all acl of these ids
-							if (error3)  res.statut(500).json({error:"error in ACL-corpusListall:", message:error3});
-							else if (dataACL != null) {
-								var dataACLLen = dataACL.length;
-								var countTreatedACL = 0;
-								var  deferredCollection = [];
-								for(var i = 0; i < dataACL.length; i++) { 
-									var foundPos = commonFuncs.findUsernameInACL(connectedUser.username, dataACL[i].users);
-									// if the user has at least a R right on this resource
-									if (foundPos != -1) {
-										if (dataACL[i].users[foundPos].right != 'N') {
-											resultReturn.push(data[i]);
-											countTreatedACL += 1; 
-										}
-									} //not found this user's right on the current resource, look for its group's one
-									else {
-										foundPos = commonFuncs.findUsernameInGroupACL(dataGroup, dataACL[i].groups);
-
-										if (foundPos != -1) {
-										 	if (dataACL[i].groups[foundPos].right != 'N') {
-												resultReturn.push(data[i]);
-												countTreatedACL += 1;
-											}
-										}
-										else { //not found user right, nor group one, do a back propagation
-											(function(d){								
-												ACL.findOne({id:req.params.id}, function(error4, dataACL1){
-													if (error4) res.status(400).json({error:"error", message:error4});
-													else if (dataACL1 != null) {
-														countTreatedACL += 1;
-														var foundPos = commonFuncs.findUsernameInACL(connectedUser.username, dataACL1.users);
-
-														if (foundPos != -1) {
-															if (dataACL1.users[foundPos].right != 'N') resultReturn.push(d);
-														}
-														else {
-															foundPos = commonFuncs.findUsernameInGroupACL(dataGroup, dataACL1.groups);
-															if (foundPos != -1 && dataACL1.groups[foundPos].right != 'N') resultReturn.push(d);
-														}
-														if (countTreatedACL == dataACLLen) {
-															countTreatedACL = -1
-															final(resultReturn, data.length);
-														}
-													}
-												}); 
-											})(data[i]);
-										} 
-									}
-								} 
-								if (countTreatedACL == dataACLLen) 
-									final(resultReturn, data.length);			
-							} 
-							else res.status(400).json({error: "error in finding acl"});
-						}); 
-					} 
-				}); 
-			} 
-			else { 
-				if (data != null) res.status(403).json({error:"You dont have permission to access this resource"}); 
-				else return([]);
-			}
-		}
 var async = require('async');
 var commonFuncs = require('../lib/commonFuncs');
 
@@ -130,15 +33,6 @@ exports.exist = function(req, res, next) {
 		else if (!media) res.status(400).json({message:"id_media don't exists"});
 		else next();
 	});
-} 
-
-//for the uri: app.get('/corpus/:id_corpus/media/:id_media', 
-exports.listWithId = function(req, res) {
-
-
-						  
-
-	console.log('ici');
 }
 
 // check if req.session.user._id have the good right to see this media.id_corpus
@@ -174,29 +68,6 @@ exports.AllowUser = function (list_right){
 	}
 }
 
-//test for Posting corpus
-//app.post('/corpus/:id_corpus/media', 
-exports.post = function(req, res){
-	if (req.body.name == undefined) return res.status(400).json({error:"one or more data fields are not filled out properly"});	
-	Corpus.findById(req.params.id_corpus, function(error, data){
-		if (error) res.status(400).json({error:"error", message:error});
-		else if (data == null) res.status(400).json({error:'Could not post this media because the given id_corpus is incorrect'});
-		else {
-			var media_data = {id_corpus: req.params.id_corpus, name: req.body.name, url : ""};
-			if (req.body.url) media_data.url = req.body.url;
-			var media = new Media(media_data);
-			// create a new media
-			media.save(function(error2, data1){
-				if (error2) res.status(400).json({error:"error", message:error2});
-				else {
-					//add the current user to the ACL list
-					var connectedUser = "root";
-					if (req.session.user) connectedUser = req.session.user.username;
-					ACLAPI.addUserRightGeneric(data1._id, connectedUser, 'A');
-					res.status(200).json(data1);
-				}
-			});
-		}
 //retrieve a particular user (with id)
 exports.getInfo = function(req, res){
 	Media.findById(req.params.id_media, 'name description id_corpus url history', function(error, media){
